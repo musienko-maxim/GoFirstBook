3acd23807b8ce40fb2afe16965425f14921e8b44
musienko maksym <mmusiien@redhat.com>
2023-05-26 00:00:53 +0300

    Add samples to chapter1

--- chapter8/literals-structure.go
+++ chapter8/literals-structure.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
-    "github.com/headfirstgo/magazine"
+	//"github.com/headfirstgo/magazine"
 )
 
-func main()  {
-	subscriber := magazine.Subscriber{Name: "Aman Singh", Rate: 4.99, Active: true}
-	fmt.Println("Name:", subscriber.Name)
-	fmt.Println("Rate:", subscriber.Rate)
-	fmt.Println("Active:", subscriber.Active)}
+func main() {
+	//subscriber := magazine.Subscriber{Name: "Aman Singh", Rate: 4.99, Active: true}
+	fmt.Println("Name:")
+	//fmt.Println("Name:", subscriber.Name)
+	//
+	//fmt.Println("Rate:", subscriber.Rate)
+	//fmt.Println("Active:", subscriber.Active)}
+}
--- chapter8/pool.go
+++ chapter8/pool.go
@@ -1,10 +1,11 @@
 package main
 
 import "fmt"
-import "github.com/headfirstgo/geo"
+
+//import "github.com/headfirstgo/geo"
 
 func main() {
-	location := geo.Coordinates{Latitude: 37.42, Longitude: -122.08}
-	fmt.Println("Latitude:", location.Latitude)
-	fmt.Println("Longitude:", location.Longitude)
+	//location := geo.Coordinates{Latitude: 37.42, Longitude: -122.08}
+	fmt.Println("Latitude:", "")
+	//fmt.Println("Longitude:",// location.Longitude)
 }
